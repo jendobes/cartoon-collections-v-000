e76ee8a97534c150f871a09752192187793cac3e
Jennifer Dobak <jdobak1@gmail.com>
2018-01-17 17:14:41 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,5 +14,5 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.find {|food| food.include?(cheese_types.join)}
+  array.find {|food| cheese_types.include?(food)}
 end
